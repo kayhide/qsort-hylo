02daa855a28b1c5f8c52b39f3ce2efc56c82ee5b
Hideaki Kawai <kayhide@gmail.com>
2020-03-01 23:57:35 +0900

    See what is happening

--- src/DevMain.hs
+++ src/DevMain.hs
@@ -2,16 +2,39 @@ module DevMain where
 
 import ClassyPrelude
 
+import Data.List (cycle)
 import Data.Functor.Foldable (hylo)
 
 run :: IO ()
-run =
-  putStrLn $ tshow $ qsort ([1,8,2,9] :: [Int])
+run = do
+  let xs = [3,2,9,8] :: [Int]
+
+  sayShow $ xs
+  sayShow $ split' xs
+  sayShow $ (fmap split' . split' $ xs)
+  sayShow $ (fmap (fmap split') . fmap split' . split' $ xs)
+  sayShow $ fmap (fmap (fmap split')) . fmap (fmap split') . fmap split' . split' $ xs
+  sayShow $ fmap (fmap (fmap join')) . fmap (fmap (fmap split')) . fmap (fmap split') . fmap split' . split' $ xs
+  sayShow $ fmap (fmap join') . fmap (fmap (fmap join')) . fmap (fmap (fmap split')) . fmap (fmap split') . fmap split' . split' $ xs
+  sayShow $ fmap join' . fmap (fmap join') . fmap (fmap (fmap join')) . fmap (fmap (fmap split')) . fmap (fmap split') . fmap split' . split' $ xs
+  sayShow $ join' . fmap join' . fmap (fmap join') . fmap (fmap (fmap join')) . fmap (fmap (fmap split')) . fmap (fmap split') . fmap split' . split' $ xs
+
+  say ""
+  sayShow $ (join' . fmap id . split') xs
+  sayShow $ (join' . fmap (join' . fmap id . split') . split') xs
+  sayShow $ (join' . fmap (join' . fmap (join' . fmap id . split') . split') . split') xs
+
+  say ""
+  let h = join' . fmap h . split'
+  sayShow $ h xs
+
+  say ""
+  sayShow $ qsort xs
 
 
 
 data TreeF a r = Leaf | Node a r r
-  deriving Functor
+  deriving (Show, Functor)
 
 split' :: Ord a => [a] -> TreeF a [a]
 split' = \case
