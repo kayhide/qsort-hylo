2abd32c0e1d98499f231339abd3c35f0786831bf
Hideaki Kawai <kayhide@gmail.com>
2020-03-01 22:59:49 +0900

    Implement qsort with hylomorphism

--- src/DevMain.hs
+++ src/DevMain.hs
@@ -2,6 +2,28 @@ module DevMain where
 
 import ClassyPrelude
 
+import Data.Functor.Foldable (hylo)
+
 run :: IO ()
 run =
-  putStrLn "[dev] Hello World"
+  putStrLn $ tshow $ qsort ([1,8,2,9] :: [Int])
+
+
+
+data TreeF a r = Leaf | Node a r r
+  deriving Functor
+
+split' :: Ord a => [a] -> TreeF a [a]
+split' = \case
+  [] -> Leaf
+  x : xs -> Node x l r
+    where
+      (l, r) = partition (< x) xs
+
+join' :: TreeF a [a] -> [a]
+join' = \case
+  Leaf       -> []
+  Node x l r -> l <> [x] <> r
+
+qsort :: Ord a => [a] -> [a]
+qsort = hylo join' split'
